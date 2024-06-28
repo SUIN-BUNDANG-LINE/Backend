93a12ce0da95bb597e81954525cb3da34e6c2c69
JeongHunHui <deca3248@gmail.com>
2024-06-29 02:20:04 +0900

    [SBL-7] 테스트 코드 실행 시 DB 연결이 안되어 오류가 발생하므로 일시적으로 contextLoads 메서드 삭제

--- src/test/kotlin/com/sbl/sulmun2yong/Sulmun2yongApplicationTests.kt
+++ src/test/kotlin/com/sbl/sulmun2yong/Sulmun2yongApplicationTests.kt
@@ -1,11 +1,6 @@
 package com.sbl.sulmun2yong
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class Sulmun2yongApplicationTests {
-    @Test
-    fun contextLoads() {
-    }
-}
+class Sulmun2yongApplicationTests
