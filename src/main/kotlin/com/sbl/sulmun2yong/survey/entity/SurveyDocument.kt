3d1321b7943a050b7a2d45609490c07031043ca9
JeongHunHui <deca3248@gmail.com>
2024-07-09 01:52:31 +0900

    [SBL-28] 설문 컬렉션 구현

---
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/entity/SurveyDocument.kt
@@ -0,0 +1,22 @@
+package com.sbl.sulmun2yong.survey.entity
+
+import com.sbl.sulmun2yong.global.entity.BaseTimeDocument
+import com.sbl.sulmun2yong.survey.domain.SurveyStatus
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.Date
+import java.util.UUID
+
+@Document(collection = "surveys")
+data class SurveyDocument(
+    @Id
+    val id: UUID,
+    var firstSectionId: UUID,
+    var status: SurveyStatus,
+    var title: String,
+    var description: String,
+    var thumbnail: String,
+    var finishMessage: String,
+    var targetParticipants: Int,
+    var enddate: Date,
+) : BaseTimeDocument()
