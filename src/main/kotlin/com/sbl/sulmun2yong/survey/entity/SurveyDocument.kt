f902ae241c97a2e43eb9871ed7f9fb3592ffffe4
JeongHunHui <deca3248@gmail.com>
2024-07-11 03:37:10 +0900

    [SBL-28] SurveyDocument에 리워드 정보까지 넣도록 수정, Question과 Section Document는 제거

--- src/main/kotlin/com/sbl/sulmun2yong/survey/entity/SurveyDocument.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/entity/SurveyDocument.kt
@@ -4,19 +4,26 @@ import com.sbl.sulmun2yong.global.entity.BaseTimeDocument
 import com.sbl.sulmun2yong.survey.domain.SurveyStatus
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import java.util.Date
 import java.util.UUID
 
+// TODO: Section 추가하기
 @Document(collection = "surveys")
 data class SurveyDocument(
     @Id
     val id: UUID,
-    var firstSectionId: UUID,
-    var status: SurveyStatus,
-    var title: String,
-    var description: String,
-    var thumbnail: String,
-    var finishMessage: String,
-    var targetParticipants: Int,
-    var endDate: Date,
-) : BaseTimeDocument()
+    val title: String,
+    val description: String,
+    val thumbnail: String,
+    val endDate: String,
+    val status: SurveyStatus,
+    val finishMessage: String,
+    val targetParticipants: Int?,
+    val rewards: List<RewardSubDocument>,
+) : BaseTimeDocument() {
+    data class RewardSubDocument(
+        val id: UUID,
+        val name: String,
+        val category: String,
+        val count: Int,
+    )
+}
