bd7ae3da44f2b41bc8bde0500032df3b3567201a
JeongHunHui <deca3248@gmail.com>
2024-07-11 18:11:08 +0900

    [SBL-28] 설문 목록 조회 API의 응답 DTO인 SurveyListResponse 구현

---
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/dto/response/SurveyListResponse.kt
@@ -0,0 +1,61 @@
+package com.sbl.sulmun2yong.survey.dto.response
+
+import com.sbl.sulmun2yong.survey.domain.Reward
+import com.sbl.sulmun2yong.survey.domain.Survey
+import java.util.Date
+import java.util.UUID
+
+data class SurveyListResponse(
+    val pageCount: Int,
+    val surveys: List<SurveyInfoResponse>,
+) {
+    companion object {
+        fun of(
+            pageCount: Int,
+            surveys: List<Survey>,
+        ): SurveyListResponse {
+            return SurveyListResponse(
+                pageCount = pageCount,
+                surveys =
+                    surveys.map {
+                        SurveyInfoResponse(
+                            surveyId = it.id,
+                            thumbnail = it.thumbnail,
+                            title = it.title,
+                            description = it.description,
+                            targetParticipants = it.targetParticipants,
+                            endDate = it.endDate,
+                            rewardCount = it.getRewardCount(),
+                            rewards = it.rewards.toRewardInfoResponses(),
+                        )
+                    },
+            )
+        }
+    }
+
+    data class SurveyInfoResponse(
+        val surveyId: UUID,
+        val thumbnail: String,
+        val title: String,
+        val description: String,
+        val targetParticipants: Int?,
+        val rewardCount: Int,
+        val endDate: Date,
+        val rewards: List<RewardInfoResponse>,
+    )
+
+    data class RewardInfoResponse(
+        val category: String,
+        val items: List<String>,
+    )
+}
+
+private fun List<Reward>.toRewardInfoResponses(): List<SurveyListResponse.RewardInfoResponse> {
+    return this.groupBy { it.category }
+        .map { (category, rewardsInCategory) ->
+            SurveyListResponse.RewardInfoResponse(
+                category = category,
+                items = rewardsInCategory.map { it.name },
+            )
+        }
+}
