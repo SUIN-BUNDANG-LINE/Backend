7d0b54d53f37cdea9e1c5753445e6499cc7a9ec4
JeongHunHui <deca3248@gmail.com>
2024-07-12 03:47:03 +0900

    [SBL-29] 설문을 찾지 못했을 때 반환할 예외 추가

--- src/main/kotlin/com/sbl/sulmun2yong/global/error/ErrorCode.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/global/error/ErrorCode.kt
@@ -7,6 +7,6 @@ enum class ErrorCode(val httpStatus: HttpStatus, val code: String, val message: 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GL0001", "서버 오류가 발생했습니다."),
     INPUT_INVALID_VALUE(HttpStatus.BAD_REQUEST, "GL0002", "잘못된 입력입니다."),
 
-    // Demo (DM)
-    DEMO_NOT_FOUND(HttpStatus.NOT_FOUND, "DM0001", "데모를 찾을 수 없습니다."),
+    // Survey (SV)
+    SURVEY_NOT_FOUND(HttpStatus.NOT_FOUND, "SV0002", "설문을 찾을 수 없습니다."),
 }
---
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/exception/SurveyNotFoundException.kt
@@ -0,0 +1,6 @@
+package com.sbl.sulmun2yong.survey.exception
+
+import com.sbl.sulmun2yong.global.error.BusinessException
+import com.sbl.sulmun2yong.global.error.ErrorCode
+
+class SurveyNotFoundException : BusinessException(ErrorCode.SURVEY_NOT_FOUND)
