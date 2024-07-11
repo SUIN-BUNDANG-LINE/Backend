49855bca6b4aae6b2f4c8d946a8c5406670d97e2
JeongHunHui <deca3248@gmail.com>
2024-07-11 18:06:20 +0900

    [SBL-28] QuestionType, SectionRouteType 제거

--- src/main/kotlin/com/sbl/sulmun2yong/survey/domain/QuestionType.kt
+++
@@ -1,7 +0,0 @@
-package com.sbl.sulmun2yong.survey.domain
-
-enum class QuestionType {
-    TEXT_RESPONSE,
-    SINGLE_CHOICE,
-    MULTIPLE_CHOICE,
-}
--- src/main/kotlin/com/sbl/sulmun2yong/survey/domain/SectionRouteType.kt
+++
@@ -1,7 +0,0 @@
-package com.sbl.sulmun2yong.survey.domain
-
-enum class SectionRouteType {
-    NUMERICAL_ORDER,
-    SET_BY_CHOICE,
-    SET_BY_USER,
-}
