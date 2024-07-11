e36a2cd17e4bc4bc9541c8c7c83521e6fda6fc2c
JeongHunHui <deca3248@gmail.com>
2024-07-12 03:48:12 +0900

    [SBL-29] 설문의 정보를 반환하는 ResponseDTO 클래스 구현

---
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/dto/response/SurveyInfoResponse.kt
@@ -0,0 +1,43 @@
+package com.sbl.sulmun2yong.survey.dto.response
+
+import com.sbl.sulmun2yong.survey.domain.Reward
+import com.sbl.sulmun2yong.survey.domain.Survey
+import com.sbl.sulmun2yong.survey.domain.SurveyStatus
+import java.util.Date
+
+data class SurveyInfoResponse(
+    val title: String,
+    val description: String,
+    val status: SurveyStatus,
+    val finishedAt: Date,
+    val currentParticipants: Int,
+    val targetParticipants: Int?,
+    val thumbnail: String,
+    val rewards: List<RewardInfoResponse>,
+) {
+    companion object {
+        fun of(survey: Survey) =
+            SurveyInfoResponse(
+                title = survey.title,
+                description = survey.description,
+                status = survey.status,
+                finishedAt = survey.finishedAt,
+                // TODO: 실제 현재 참여자 값 넣도록 구현하기
+                currentParticipants = 0,
+                targetParticipants = survey.targetParticipants,
+                thumbnail = survey.thumbnail,
+                rewards = survey.rewards.map { it.toResponse() },
+            )
+    }
+
+    data class RewardInfoResponse(
+        val item: String,
+        val count: Int,
+    )
+}
+
+private fun Reward.toResponse() =
+    SurveyInfoResponse.RewardInfoResponse(
+        item = this.name,
+        count = this.count,
+    )
