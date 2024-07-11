d5063afbe4ef8a08d43fa54ddb221c54585b9bfe
JeongHunHui <deca3248@gmail.com>
2024-07-12 03:48:49 +0900

    [SBL-29] 설문 정보 조회 API 구현

--- src/main/kotlin/com/sbl/sulmun2yong/survey/controller/SurveyController.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/controller/SurveyController.kt
@@ -2,12 +2,16 @@ package com.sbl.sulmun2yong.survey.controller
 
 import com.sbl.sulmun2yong.survey.controller.doc.SurveyInfoApiDoc
 import com.sbl.sulmun2yong.survey.dto.request.SurveySortType
+import com.sbl.sulmun2yong.survey.dto.response.SurveyInfoResponse
 import com.sbl.sulmun2yong.survey.dto.response.SurveyListResponse
 import com.sbl.sulmun2yong.survey.service.SurveyInfoService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
+import java.util.UUID
 
 @Controller
 class SurveyController(private val surveyInfoService: SurveyInfoService) : SurveyInfoApiDoc {
@@ -21,5 +25,13 @@ class SurveyController(private val surveyInfoService: SurveyInfoService) : Surve
         return ResponseEntity.ok(
             surveyInfoService.getSurveysWithPagination(size, page, sortType, isAsc),
         )
+    }
+
+    @Operation(summary = "설문 정보 조회")
+    @GetMapping("/surveys/info/{survey-id}")
+    override fun getSurveyInfo(
+        @PathVariable("survey-id") surveyId: UUID,
+    ): ResponseEntity<SurveyInfoResponse> {
+        return ResponseEntity.ok(surveyInfoService.getSurvey(surveyId))
     }
 }
