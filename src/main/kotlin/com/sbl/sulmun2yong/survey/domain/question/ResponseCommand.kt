34c6c782fc70bc329c67fb184c2074babd753de2
JeongHunHui <deca3248@gmail.com>
2024-07-15 20:43:25 +0900

    [SBL-36] ResponseCommand가 테스트 코드를 충족 시키도록 유효성 검사 구현

--- src/main/kotlin/com/sbl/sulmun2yong/survey/domain/question/ResponseCommand.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/domain/question/ResponseCommand.kt
@@ -1,8 +1,19 @@
 package com.sbl.sulmun2yong.survey.domain.question
 
+import com.sbl.sulmun2yong.survey.exception.InvalidResponseCommandException
+
 data class ResponseCommand(
     val responseDetails: List<ResponseDetail>,
-)
+) {
+    init {
+        // 응답은 비어있으면 안된다
+        if (responseDetails.isEmpty()) throw InvalidResponseCommandException()
+        // 응답의 내용은 중복되면 안된다
+        if (responseDetails.size != responseDetails.distinctBy { it }.size) throw InvalidResponseCommandException()
+        // 기타 응답은 하나보다 많이 올 수 없다
+        if (responseDetails.count { it.isEtc } > 1) throw InvalidResponseCommandException()
+    }
+}
 
 data class ResponseDetail(
     val content: String,
