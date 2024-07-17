3d35aba745c7620a648de89c48536cf87f6c36a1
JeongHunHui <deca3248@gmail.com>
2024-07-18 00:18:33 +0900

    [SBL-36] 섹션 생성 시 섹션이 유효한지 확인하는 코드 추가, 해당 부분 테스트 통과

--- src/main/kotlin/com/sbl/sulmun2yong/survey/domain/Section.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/domain/Section.kt
@@ -1,6 +1,9 @@
 package com.sbl.sulmun2yong.survey.domain
 
 import com.sbl.sulmun2yong.survey.domain.question.Question
+import com.sbl.sulmun2yong.survey.domain.question.QuestionType
+import com.sbl.sulmun2yong.survey.domain.question.SingleChoiceQuestion
+import com.sbl.sulmun2yong.survey.exception.InvalidSectionException
 import java.util.UUID
 
 data class Section(
@@ -11,7 +14,19 @@ data class Section(
     val questions: List<Question>,
 ) {
     init {
-        // TODO: RouteDetails와 섹션의 정보들이 유요한지 확인
+        if (routeDetails is RouteDetails.SetByChoice) {
+            val keyQuestion =
+                questions.find { it.id == routeDetails.keyQuestionId }
+                    ?: throw InvalidSectionException()
+            if (keyQuestion.questionType != QuestionType.SINGLE_CHOICE) throw InvalidSectionException()
+            if (!keyQuestion.isRequired) throw InvalidSectionException()
+            val isValidSectionConfigs =
+                routeDetails.isValidSectionRouteConfig(
+                    keyQuestion.isAllowOther,
+                    (keyQuestion as SingleChoiceQuestion).choices,
+                )
+            if (isValidSectionConfigs.not()) throw InvalidSectionException()
+        }
     }
 
     fun findNextSectionId(sectionResponses: List<SectionResponse>): UUID? {
