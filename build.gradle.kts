8b86bc1e4e0084396d3d8d96a2f3d39eadc8d907
JeongHunHui <deca3248@gmail.com>
2024-07-17 22:57:18 +0900

    [SBL-36] Mockito 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,9 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
     // test
+    testImplementation("org.mockito:mockito-core:4.0.0")
+    testImplementation("org.mockito:mockito-inline:4.0.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
