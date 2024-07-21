49a8464937c981b404d16b54f73096f5d94f396b
JeongHunHui <deca3248@gmail.com>
2024-07-21 19:47:16 +0900

    [SBL-36] 리워드의 count가 음수가될 수 없도록 유효성 검증 로직 및 테스트 코드 작성

--- src/main/kotlin/com/sbl/sulmun2yong/global/error/ErrorCode.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/global/error/ErrorCode.kt
@@ -13,13 +13,14 @@ enum class ErrorCode(
 
     // Survey (SV)
     SURVEY_NOT_FOUND(HttpStatus.NOT_FOUND, "SV0002", "설문을 찾을 수 없습니다."),
+    INVALID_SECTION_ROUTE_CONFIGS(HttpStatus.BAD_REQUEST, "SV0003", "유효하지 않은 섹션 루트 설정입니다."),
     INVALID_QUESTION_RESPONSE(HttpStatus.BAD_REQUEST, "SV0004", "유효하지 않은 질문 응답입니다."),
     INVALID_SECTION(HttpStatus.BAD_REQUEST, "SV0005", "유효하지 않은 섹션입니다."),
     INVALID_SECTION_RESPONSE(HttpStatus.BAD_REQUEST, "SV0006", "유효하지 않은 섹션 응답입니다."),
     INVALID_SURVEY(HttpStatus.BAD_REQUEST, "SV0008", "유효하지 않은 설문입니다."),
     INVALID_SURVEY_RESPONSE(HttpStatus.BAD_REQUEST, "SV0009", "유효하지 않은 설문 응답입니다."),
     INVALID_CHOICE(HttpStatus.BAD_REQUEST, "SV0010", "유효하지 않은 선택지입니다."),
-    INVALID_SECTION_ROUTE_CONFIGS(HttpStatus.BAD_REQUEST, "SV0003", "유효하지 않은 섹션 루트 설정입니다."),
+    INVALID_REWARD(HttpStatus.BAD_REQUEST, "SV0011", "유효하지 않은 리워드 정보 입니다."),
 
     // OAuth2 (OA)
     PROVIDER_NOT_FOUND(HttpStatus.NOT_FOUND, "OA0001", "지원하지 않는 소셜 로그인입니다."),
--- src/main/kotlin/com/sbl/sulmun2yong/survey/domain/Reward.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/domain/Reward.kt
@@ -1,5 +1,6 @@
 package com.sbl.sulmun2yong.survey.domain
 
+import com.sbl.sulmun2yong.survey.exception.InvalidRewardException
 import java.util.UUID
 
 data class Reward(
@@ -7,4 +8,8 @@ data class Reward(
     val name: String,
     val category: String,
     val count: Int,
-)
+) {
+    init {
+        require(count > 0) { throw InvalidRewardException() }
+    }
+}
