28998c5b1886d4a912afd9e62b23c49a6912bb42
JeongHunHui <deca3248@gmail.com>
2024-07-24 01:36:30 +0900

    [SBL-52] 설문 응답 API 구현

---
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/controller/SurveyResponseController.kt
@@ -0,0 +1,25 @@
+package com.sbl.sulmun2yong.survey.controller
+
+import com.sbl.sulmun2yong.survey.controller.doc.SurveyResponseApiDoc
+import com.sbl.sulmun2yong.survey.dto.request.SurveyResponseRequest
+import com.sbl.sulmun2yong.survey.dto.response.SurveyParticipantResponse
+import com.sbl.sulmun2yong.survey.service.SurveyResponseService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+@RequestMapping("/api/v1/surveys/response")
+class SurveyResponseController(private val surveyResponseService: SurveyResponseService) : SurveyResponseApiDoc {
+    @PostMapping("/{survey-id}")
+    override fun responseToSurvey(
+        @PathVariable("survey-id") surveyId: UUID,
+        @RequestBody surveyResponseRequest: SurveyResponseRequest,
+    ): ResponseEntity<SurveyParticipantResponse> {
+        return ResponseEntity.ok(surveyResponseService.responseToSurvey(surveyId, surveyResponseRequest))
+    }
+}
