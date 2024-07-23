b30509efcd41d50946ffbe95accb025832e8cb83
JeongHunHui <deca3248@gmail.com>
2024-07-24 01:34:16 +0900

    [SBL-52] 설문의 응답을 받는 RequestDTO 구현

---
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/dto/request/SurveyResponseRequest.kt
@@ -0,0 +1,46 @@
+package com.sbl.sulmun2yong.survey.dto.request
+
+import com.sbl.sulmun2yong.survey.domain.SectionResponse
+import com.sbl.sulmun2yong.survey.domain.SurveyResponse
+import com.sbl.sulmun2yong.survey.domain.question.QuestionResponse
+import com.sbl.sulmun2yong.survey.domain.question.ResponseDetail
+import java.util.UUID
+
+data class SurveyResponseRequest(
+    val sectionResponses: List<SectionResponseRequest>,
+) {
+    data class SectionResponseRequest(
+        val sectionId: UUID,
+        val questionResponses: List<QuestionResponseRequest>,
+    ) {
+        data class QuestionResponseRequest(
+            val questionId: UUID,
+            val responses: List<ResponseDetailRequest>,
+        ) {
+            data class ResponseDetailRequest(
+                val content: String,
+                val isOther: Boolean,
+            ) {
+                fun toDomain() =
+                    ResponseDetail(
+                        content = content,
+                        isOther = isOther,
+                    )
+            }
+
+            fun toDomain() =
+                QuestionResponse(
+                    questionId = questionId,
+                    responses = responses.map { it.toDomain() },
+                )
+        }
+
+        fun toDomain() =
+            SectionResponse(
+                sectionId = sectionId,
+                questionResponses = questionResponses.map { it.toDomain() },
+            )
+    }
+
+    fun toDomain(surveyId: UUID) = SurveyResponse(surveyId = surveyId, sectionResponses = sectionResponses.map { it.toDomain() })
+}
