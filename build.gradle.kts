467280fba9e3e068cdf1457f1c9dfd3605998975
JeongHunHui <deca3248@gmail.com>
2024-07-26 05:30:23 +0900

    [SBL-63] sonarqube에 필요한 xml형식의 report를 만들도록 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,8 +80,8 @@ tasks.test {
 
 tasks.jacocoTestReport {
     reports {
-        html.required.set(true)
-        xml.required.set(false)
+        html.required.set(false)
+        xml.required.set(true)
         csv.required.set(false)
     }
 }
