baff2ad359a9c788f8c2dcb091e22df5ca6e90ca
Oh YoungJe <pine7420@naver.com>
2024-07-30 21:00:31 +0900

    [SBL-69] 불필요한 어노테이션 제거

--- src/main/kotlin/com/sbl/sulmun2yong/user/dto/DefaultUserProfile.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/user/dto/DefaultUserProfile.kt
@@ -1,13 +1,12 @@
 package com.sbl.sulmun2yong.user.dto
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.util.Base64
 import java.util.UUID
 
 data class DefaultUserProfile(
-    @JsonProperty("id") val id: UUID,
-    @JsonProperty("nickname") val nickname: String,
+    val id: UUID,
+    val nickname: String,
 ) {
     fun toBase64Json(): String {
         val objectMapper = ObjectMapper()
