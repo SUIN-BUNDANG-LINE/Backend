86bae86cbe10e81b8852842e5bebea53497d50a8
JeongHunHui <deca3248@gmail.com>
2024-08-01 02:55:43 +0900

    [SBL-56] 선택지 관련 클래스에 주석 추가

--- src/main/kotlin/com/sbl/sulmun2yong/survey/domain/question/choice/Choice.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/domain/question/choice/Choice.kt
@@ -1,5 +1,6 @@
 package com.sbl.sulmun2yong.survey.domain.question.choice
 
+/** 선택지(Standard: 일반 선택지, Other: 기타 선택지) */
 sealed class Choice {
     abstract val content: String?
 
@@ -7,10 +8,12 @@ sealed class Choice {
         fun from(content: String?) = if (content == null) Other else Standard(content)
     }
 
+    /** 일반 선택지 */
     data class Standard(
         override val content: String,
     ) : Choice()
 
+    /** 기타 선택지 */
     data object Other : Choice() {
         override val content = null
     }
--- src/main/kotlin/com/sbl/sulmun2yong/survey/domain/question/choice/Choices.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/domain/question/choice/Choices.kt
@@ -3,8 +3,10 @@ package com.sbl.sulmun2yong.survey.domain.question.choice
 import com.sbl.sulmun2yong.survey.domain.response.ResponseDetail
 import com.sbl.sulmun2yong.survey.exception.InvalidChoiceException
 
+/** 질문에 포함될 선택지 목록 */
 data class Choices(
     val standardChoices: List<Choice.Standard>,
+    /** 기타 선택지 허용 여부 */
     val isAllowOther: Boolean,
 ) {
     init {
