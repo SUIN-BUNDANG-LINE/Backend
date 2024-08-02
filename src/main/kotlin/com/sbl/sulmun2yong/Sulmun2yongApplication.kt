e3749cabeba007d4e36597454916d454e9cb3f53
JeongHunHui <deca3248@gmail.com>
2024-08-03 05:08:38 +0900

    HOTFIX: swagger url 추가, favicon.ico 추가

--- src/main/kotlin/com/sbl/sulmun2yong/Sulmun2yongApplication.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/Sulmun2yongApplication.kt
@@ -1,8 +1,11 @@
 package com.sbl.sulmun2yong
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(servers = [Server(url = "\${backend.base-url}", description = "설문이용 서버")])
 @SpringBootApplication
 class Sulmun2yongApplication
 
