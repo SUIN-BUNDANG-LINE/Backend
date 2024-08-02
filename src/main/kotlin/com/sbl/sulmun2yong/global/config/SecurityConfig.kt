2eedf9da25791c9c34c125abafaa9d5516a000a0
JeongHunHui <deca3248@gmail.com>
2024-08-03 00:40:16 +0900

    [SBL-74] swagger 조회를 위해 formlogin을 하게 설정 할 수 있도록 하는 기능 구현

--- src/main/kotlin/com/sbl/sulmun2yong/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/global/config/SecurityConfig.kt
@@ -6,13 +6,21 @@ import com.sbl.sulmun2yong.global.config.oauth2.handler.CustomLogoutSuccessHandl
 import com.sbl.sulmun2yong.global.config.oauth2.strategy.CustomExpiredSessionStrategy
 import com.sbl.sulmun2yong.global.config.oauth2.strategy.CustomInvalidSessionStrategy
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.core.session.SessionRegistry
 import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
 
@@ -20,12 +28,48 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint
 class SecurityConfig(
     @Value("\${frontend.base-url}")
     private val baseUrl: String,
-    @Value("\${swagger.oauth2}")
-    private val isSwaggerNeedOAuth2: Boolean?,
+    @Value("\${swagger.username}")
+    private val username: String?,
+    @Value("\${swagger.password}")
+    private val password: String?,
 ) {
     @Bean
     fun sessionRegistry(): SessionRegistry = SessionRegistryImpl()
 
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user =
+            User
+                .builder()
+                .username(username)
+                .password(passwordEncoder().encode(password))
+                .roles("SWAGGER_USER")
+                .build()
+        return InMemoryUserDetailsManager(user)
+    }
+
+    @ConditionalOnProperty(prefix = "swagger", name = ["login"], havingValue = "true")
+    @Order(0)
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { requests ->
+                requests
+                    .requestMatchers("/swagger-ui/**")
+                    .hasRole("SWAGGER_USER")
+                    .requestMatchers("/v3/api-docs/**")
+                    .hasRole("SWAGGER_USER")
+                    .requestMatchers("/**")
+                    .permitAll()
+            }.formLogin(Customizer.withDefaults())
+
+        return http.build()
+    }
+
+    @Order(1)
     @Bean
     fun securityFilterChain(
         http: HttpSecurity,
@@ -47,10 +91,6 @@ class SecurityConfig(
                 logoutSuccessHandler = CustomLogoutSuccessHandler(baseUrl, sessionRegistry())
             }
             authorizeHttpRequests {
-                if (isSwaggerNeedOAuth2 != null && isSwaggerNeedOAuth2) {
-                    authorize("/swagger-ui/**", hasRole("ADMIN"))
-                    authorize("/v3/api-docs/**", hasRole("ADMIN"))
-                }
                 authorize("/api/v1/admin/**", hasRole("ADMIN"))
                 authorize("/api/v1/user/**", authenticated)
                 authorize("/**", permitAll)
