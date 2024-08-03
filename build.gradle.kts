32e4a9fc820210f50d1c24d74544ccc58c72b06e
Oh YoungJe <pine7420@naver.com>
2024-08-03 15:32:30 +0900

    health-check-route

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     // security
     implementation("org.springframework.boot:spring-boot-starter-security")
