23b2e9bc31558e272dd32aa3d2b71c69af107114
JeongHunHui <deca3248@gmail.com>
2024-08-08 01:02:02 +0900

    [SBL-80] 설문 정보 업데이트 시 발생할 예외 클래스 구현

---
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/exception/InvalidUpdateSurveyException.kt
@@ -0,0 +1,6 @@
+package com.sbl.sulmun2yong.survey.exception
+
+import com.sbl.sulmun2yong.global.error.BusinessException
+import com.sbl.sulmun2yong.global.error.ErrorCode
+
+class InvalidUpdateSurveyException : BusinessException(ErrorCode.INVALID_UPDATE_SURVEY)
