59261f0805ec9c0cacde837be57fbe2927db97e5
Oh YoungJe <pine7420@naver.com>
2024-08-08 21:58:17 +0900

    [SBL-78] 머지 후 불필요 해진 패키지 제거

--- src/main/kotlin/com/sbl/sulmun2yong/rewarddrawing/exception/InvalidDrawingBoardException.kt
+++
@@ -1,6 +0,0 @@
-package com.sbl.sulmun2yong.rewarddrawing.exception
-
-import com.sbl.sulmun2yong.global.error.BusinessException
-import com.sbl.sulmun2yong.global.error.ErrorCode
-
-class InvalidDrawingBoardException : BusinessException(ErrorCode.INVALID_DRAWING_BOARD)
