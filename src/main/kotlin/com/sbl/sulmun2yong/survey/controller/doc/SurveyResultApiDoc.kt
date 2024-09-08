0186b0185dfcc881a43f0d1854e55b75c4f2ff89
JeongHunHui <deca3248@gmail.com>
2024-09-08 22:47:05 +0900

    [SBL-123] 설문 결과 조회 API에 필터링 기능 추가

--- src/main/kotlin/com/sbl/sulmun2yong/survey/controller/doc/SurveyResultApiDoc.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/controller/doc/SurveyResultApiDoc.kt
@@ -1,20 +1,23 @@
 package com.sbl.sulmun2yong.survey.controller.doc
 
 import com.sbl.sulmun2yong.global.annotation.LoginUser
+import com.sbl.sulmun2yong.survey.dto.request.SurveyResultRequest
 import com.sbl.sulmun2yong.survey.dto.response.SurveyResultResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import java.util.UUID
 
 @Tag(name = "SurveyResult", description = "설문 결과 관련 API")
 interface SurveyResultApiDoc {
     @Operation(summary = "설문 결과 조회")
-    @GetMapping("/{survey-id}")
+    @PostMapping("/{survey-id}")
     fun getSurveyResult(
         @PathVariable("survey-id") surveyId: UUID,
         @LoginUser id: UUID,
+        @RequestBody surveyResultRequest: SurveyResultRequest,
     ): ResponseEntity<SurveyResultResponse>
 }
