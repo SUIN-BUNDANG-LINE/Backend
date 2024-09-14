7aec64c3c68e5c2324b482058464dfc2cd79eb4e
JeongHunHui <deca3248@gmail.com>
2024-09-14 23:22:54 +0900

    [SBL-119] 마이페이지 설문 정보 응답 DTO 클래스 구현

---
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/dto/response/MyPageSurveyInfoResponse.kt
@@ -0,0 +1,16 @@
+package com.sbl.sulmun2yong.survey.dto.response
+
+import com.sbl.sulmun2yong.survey.domain.SurveyStatus
+import java.util.Date
+import java.util.UUID
+
+data class MyPageSurveyInfoResponse(
+    val id: UUID,
+    val title: String,
+    val thumbnail: String?,
+    val updatedAt: Date,
+    val status: SurveyStatus,
+    val publishedAt: Date?,
+    val finishedAt: Date?,
+    val responseCount: Int,
+)
