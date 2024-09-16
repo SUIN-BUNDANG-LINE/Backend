9618826c5083461f9d55badf99d3b446a641a89f
JeongHunHui <deca3248@gmail.com>
2024-09-16 17:46:55 +0900

    [SBL-138] 설문 결과 API가 참가자 ID로 필터링을 할 수 있고, 설문의 정보도 포함 시키도록 수정

--- src/main/kotlin/com/sbl/sulmun2yong/survey/service/SurveyResultService.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/service/SurveyResultService.kt
@@ -4,7 +4,6 @@ import com.sbl.sulmun2yong.survey.adapter.ResponseAdapter
 import com.sbl.sulmun2yong.survey.adapter.SurveyAdapter
 import com.sbl.sulmun2yong.survey.dto.request.SurveyResultRequest
 import com.sbl.sulmun2yong.survey.dto.response.SurveyResultResponse
-import com.sbl.sulmun2yong.survey.exception.SurveyNotFoundException
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -15,19 +14,18 @@ class SurveyResultService(
 ) {
     fun getSurveyResult(
         surveyId: UUID,
-        userId: UUID,
+        makerId: UUID,
         surveyResultRequest: SurveyResultRequest,
+        participantId: UUID?,
     ): SurveyResultResponse {
-        val isSurveyExists = surveyAdapter.existsByIdAndMakerId(surveyId, userId)
-        // 본인이 만든 설문이 아닌 경우 예외 발생
-        if (!isSurveyExists) throw SurveyNotFoundException()
+        val survey = surveyAdapter.getByIdAndMakerId(surveyId, makerId)
 
         // DB에서 설문 결과 조회
-        val surveyResult = responseAdapter.getSurveyResult(surveyId)
+        val surveyResult = responseAdapter.getSurveyResult(surveyId, participantId)
 
         // 요청에 따라 설문 결과 필터링
         val resultFilter = surveyResultRequest.toDomain()
         val filteredSurveyResult = surveyResult.getFilteredResult(resultFilter)
-        return SurveyResultResponse.of(filteredSurveyResult)
+        return SurveyResultResponse.of(filteredSurveyResult, survey)
     }
 }
