c1052880971f57987f58ec54c3b870a4d93d6e27
JeongHunHui <deca3248@gmail.com>
2024-09-24 03:59:27 +0900

    [SBL-148] 필터는 최대 20개 까지만 적용할 수 있도록 유효성 검증 추가

---
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/exception/InvalidResultFilterException.kt
@@ -0,0 +1,6 @@
+package com.sbl.sulmun2yong.survey.exception
+
+import com.sbl.sulmun2yong.global.error.BusinessException
+import com.sbl.sulmun2yong.global.error.ErrorCode
+
+class InvalidResultFilterException : BusinessException(ErrorCode.INVALID_RESULT_FILTER)
