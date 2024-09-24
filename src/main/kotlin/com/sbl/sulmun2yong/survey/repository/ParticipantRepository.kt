f082e07bc925a40c8aafc0854883b580d3828a68
JeongHunHui <deca3248@gmail.com>
2024-09-25 03:22:41 +0900

    [SBL-146] 설문 ID로 참가자 목록을 불러오는 Adapter 메서드 구현

--- src/main/kotlin/com/sbl/sulmun2yong/survey/repository/ParticipantRepository.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/repository/ParticipantRepository.kt
@@ -3,10 +3,9 @@ package com.sbl.sulmun2yong.survey.repository
 import com.sbl.sulmun2yong.survey.entity.ParticipantDocument
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
-import java.util.Optional
 import java.util.UUID
 
 @Repository
 interface ParticipantRepository : MongoRepository<ParticipantDocument, UUID> {
-    fun findBySurveyId(surveyId: UUID): Optional<ParticipantDocument>
+    fun findBySurveyId(surveyId: UUID): List<ParticipantDocument>
 }
