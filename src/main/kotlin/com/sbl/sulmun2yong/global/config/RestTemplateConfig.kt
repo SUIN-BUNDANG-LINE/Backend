9210709a65c27d3435aee3aedce72cfb0f75f9ba
Oh YoungJe <pine7420@naver.com>
2024-09-24 14:47:00 +0900

    [SBL-139] TODO 추가

--- src/main/kotlin/com/sbl/sulmun2yong/global/config/RestTemplateConfig.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/global/config/RestTemplateConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
 
+// TODO : WebClient 시용 고려
 @Configuration
 class RestTemplateConfig {
     @Bean
