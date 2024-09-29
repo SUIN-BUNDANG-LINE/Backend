c3d1633aacd4ee6c2236f0b19428a5dc4bf7cc47
JeongHunHui <deca3248@gmail.com>
2024-09-30 03:23:20 +0900

    [SBL-165] Mongock 관련 Config 파일 추가, application.yml에 설정 추가

---
+++ src/main/kotlin/com/sbl/sulmun2yong/global/config/MongockConfig.kt
@@ -0,0 +1,8 @@
+package com.sbl.sulmun2yong.global.config
+
+import io.mongock.runner.springboot.EnableMongock
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableMongock
+class MongockConfig
