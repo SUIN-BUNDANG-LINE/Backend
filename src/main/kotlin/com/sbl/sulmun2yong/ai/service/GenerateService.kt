89b96df9cd094ceb3dd481901bef1615c27181c8
Oh YoungJe <pine7420@naver.com>
2024-10-02 15:57:18 +0900

    [SBL-169] 쿠키로 채팅세션아이디를 만들어줌

--- src/main/kotlin/com/sbl/sulmun2yong/ai/service/GenerateService.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/ai/service/GenerateService.kt
@@ -1,6 +1,8 @@
 package com.sbl.sulmun2yong.ai.service
 
 import com.sbl.sulmun2yong.ai.adapter.GenerateAdapter
+import com.sbl.sulmun2yong.ai.dto.request.SurveyGenerationWithFileUrlRequest
+import com.sbl.sulmun2yong.ai.dto.request.SurveyGenerationWithTextDocumentRequest
 import com.sbl.sulmun2yong.ai.dto.response.SurveyGenerationResponse
 import com.sbl.sulmun2yong.global.util.validator.FileUrlValidator
 import org.springframework.stereotype.Service
@@ -10,22 +12,27 @@ class GenerateService(
     private val fileUrlValidator: FileUrlValidator,
     private val generateAdapter: GenerateAdapter,
 ) {
-    fun generateSurveyWithFileUrl(
-        job: String,
-        groupName: String,
-        fileUrl: String,
-        userPrompt: String,
-    ): SurveyGenerationResponse {
+    fun generateSurveyWithFileUrl(surveyGenerationWithFileUrlRequest: SurveyGenerationWithFileUrlRequest): SurveyGenerationResponse {
         val allowedExtensions = mutableListOf(".txt", ".pdf")
+
+        val job = surveyGenerationWithFileUrlRequest.job
+        val groupName = surveyGenerationWithFileUrlRequest.groupName
+        val fileUrl = surveyGenerationWithFileUrlRequest.fileUrl
+        val userPrompt = surveyGenerationWithFileUrlRequest.userPrompt
+
         fileUrlValidator.validateFileUrlOf(fileUrl, allowedExtensions)
 
         return generateAdapter.requestSurveyGenerationWithFileUrl(job, groupName, fileUrl, userPrompt)
     }
 
     fun generateSurveyWithTextDocument(
-        job: String,
-        groupName: String,
-        textDocument: String,
-        userPrompt: String,
-    ): SurveyGenerationResponse = generateAdapter.requestSurveyGenerationWithTextDocument(job, groupName, textDocument, userPrompt)
+        surveyGenerationWithTextDocumentRequest: SurveyGenerationWithTextDocumentRequest,
+    ): SurveyGenerationResponse {
+        val job = surveyGenerationWithTextDocumentRequest.job
+        val groupName = surveyGenerationWithTextDocumentRequest.groupName
+        val textDocument = surveyGenerationWithTextDocumentRequest.textDocument
+        val userPrompt = surveyGenerationWithTextDocumentRequest.userPrompt
+
+        return generateAdapter.requestSurveyGenerationWithTextDocument(job, groupName, textDocument, userPrompt)
+    }
 }
