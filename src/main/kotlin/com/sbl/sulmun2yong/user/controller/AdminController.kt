541006e417edfa1192d5a19caf0229fe2d5e06a5
JeongHunHui <deca3248@gmail.com>
2024-10-12 05:51:33 +0900

    [SBL-190] 세션 방식을 jwt로 전환함에 따라 세션으로 로그인된 사용자 목록을 확인하는 API 제거

--- src/main/kotlin/com/sbl/sulmun2yong/user/controller/AdminController.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/user/controller/AdminController.kt
@@ -3,10 +3,8 @@ package com.sbl.sulmun2yong.user.controller
 import com.sbl.sulmun2yong.drawing.dto.response.DrawingHistoryGroupListResponse
 import com.sbl.sulmun2yong.drawing.dto.response.DrawingHistoryGroupResponse
 import com.sbl.sulmun2yong.user.controller.doc.AdminApiDoc
-import com.sbl.sulmun2yong.user.dto.response.UserSessionsResponse
 import com.sbl.sulmun2yong.user.service.AdminService
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.session.SessionRegistry
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,16 +16,8 @@ import java.util.UUID
 @RestController
 @RequestMapping("/api/v1/admin")
 class AdminController(
-    private val sessionRegistry: SessionRegistry,
     private val adminService: AdminService,
 ) : AdminApiDoc {
-    @GetMapping("/sessions/logged-in-users")
-    @ResponseBody
-    override fun getAllLoggedInUsers(): ResponseEntity<UserSessionsResponse> {
-        val allPrincipals = sessionRegistry.allPrincipals
-        return ResponseEntity.ok(adminService.getAllLoggedInUsers(allPrincipals))
-    }
-
     @GetMapping("/drawing-history/{surveyId}")
     override fun getDrawingHistory(
         @PathVariable surveyId: UUID,
