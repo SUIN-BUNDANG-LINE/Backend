03c724ce6abdf01c4aba3ea06c368423eeda06f2
JeongHunHui <deca3248@gmail.com>
2024-10-12 06:12:14 +0900

    [SBL-190] DefaultUserProfile 클래스에서 필요없는 메서드 제거

--- src/main/kotlin/com/sbl/sulmun2yong/user/dto/DefaultUserProfile.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/user/dto/DefaultUserProfile.kt
@@ -1,18 +1,10 @@
 package com.sbl.sulmun2yong.user.dto
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.sbl.sulmun2yong.user.domain.UserRole
-import java.util.Base64
 import java.util.UUID
 
 data class DefaultUserProfile(
     val id: UUID,
     val nickname: String,
     val role: UserRole,
-) {
-    fun toBase64Json(): String {
-        val objectMapper = ObjectMapper()
-        val json = objectMapper.writeValueAsString(this)
-        return Base64.getEncoder().encodeToString(json.toByteArray())
-    }
-}
+)
