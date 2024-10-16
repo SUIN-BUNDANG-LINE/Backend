5f0a3997d5f1078e2c75d461c20cd1f7484df1a6
JeongHunHui <deca3248@gmail.com>
2024-10-17 02:31:38 +0900

    [SBL-204] 무작위 값을 생성하기 위해 필요한 클래스 구현(확률에 기반하여 결과를 반환하는 메서드를 가진 클래스)

---
+++ src/main/kotlin/com/sbl/sulmun2yong/user/util/ProbabilityPicker.kt
@@ -0,0 +1,25 @@
+package com.sbl.sulmun2yong.user.util
+
+import kotlin.math.abs
+
+data class ProbabilityPicker<T>(
+    private val probabilityMap: Map<T, Double>,
+) {
+    init {
+        val totalProbability = probabilityMap.values.sum()
+        // 부동소수점 오차를 고려하여 1에 가까운지 확인
+        require(abs(totalProbability - 1.0) < 1e-9) { "확률의 합은 1이어야 합니다." }
+    }
+
+    fun pick(): T {
+        val randomValue = Math.random()
+        var totalProbability = 0.0
+
+        for ((item, probability) in probabilityMap) {
+            totalProbability += probability
+            if (randomValue <= totalProbability) return item
+        }
+
+        return probabilityMap.keys.last()
+    }
+}
